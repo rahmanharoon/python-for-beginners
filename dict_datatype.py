64344653f39e98ceaf22b1228c166c8b6aabfbe9
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-11 20:29:19 +0530

    second commit

---
+++ dict_datatype.py
@@ -0,0 +1,12 @@
+d={101:'XYZ',202:'ABC',303:'True'}
+print(d[101])
+
+print(d)
+
+f={}
+print(f)
+
+d['a']='Apple'
+d['b']='Ball'
+d['c']='Cat'
+print(d)
---
+++ none_datatype.py
@@ -0,0 +1,5 @@
+def m1():
+    a=10
+    print(m1())
+#none_datatype means nothing or  no value to print
+#if the value is not available then none is used
