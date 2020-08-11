efd453ee03d5174e92cde0bc8c116e1e0019de4e
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-11 23:13:27 +0530

    forth commit

---
+++ bitwise_operator.py
@@ -0,0 +1,9 @@
+print("4&5=",4 & 5)
+print("True&True=",True & True)
+print("4|5=",4|5)
+print("True|True=",True|True)
+print("4^5=",4^5)
+print("True^True=",True^True)
+
+print(10>>2)
+print(10<<2) 
