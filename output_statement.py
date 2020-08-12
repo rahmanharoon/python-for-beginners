9c3fc22d4848479b53d00bece858dd35644b41a4
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-12 20:28:06 +0530

    10th commit

---
+++ output_statement.py
@@ -0,0 +1,15 @@
+print(10*"Hello")
+print("Hello"*10)
+print("Hello"+"World")
+print("Hello","World")
+
+a,b,c=10,20,30
+print(a,b,c,sep=',')
+print(a,b,c,sep=':')
+
+s="ROCKY"
+h=22
+s1="JAVA"
+s2="PYTHON"
+print("Hello",s,"Your age is",h)
+print("You are studying",s1,"and",s2)
