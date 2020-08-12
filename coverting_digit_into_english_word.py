cf6fad1cb6e1009f962533b82e03128b8930f115
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-12 21:44:52 +0530

    coverting digit into english word commited

---
+++ coverting_digit_into_english_word.py
@@ -0,0 +1,25 @@
+n=int(input("Enter a Number:"))
+if n==0:
+    print("ZER0")
+elif n==1:
+    print("ONE")
+elif n==2:
+    print("TWO")
+elif n==3:
+    print("THREE")
+elif n==4:
+    print("FOUR")
+elif n==5:
+    print("FIVE")
+elif n==6:
+    print("SIX")
+elif n==7:
+    print("SEVEN")
+elif n==8:
+    print("EIGHT")
+elif n==9:
+    print("NINE")
+elif n==10:
+    print("TEN")
+else:
+    print("Linit Over")
