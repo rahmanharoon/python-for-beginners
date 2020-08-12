188cbc51157739719bdbe2323564845cb176be6a
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-12 20:35:23 +0530

    o/p statements committed

--- output_statement.py
+++ output_statement.py
@@ -13,3 +13,9 @@ s1="JAVA"
 s2="PYTHON"
 print("Hello",s,"Your age is",h)
 print("You are studying",s1,"and",s2)
+
+name="ROCKY"
+salary=18000
+place="Kerala"
+print("Hello{} your salary is {} and you are from {}".format(name,salary,place))
+print("Hello{x} your salary is {y} and you are from{z}".format(x=name,y=salary,z=place))
