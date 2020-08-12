3a539086fb0d5951164f92df05e2cc1b090f4f3b
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-12 14:54:05 +0530

    command line arguments commited

---
+++ cmd_line_arg.py
@@ -0,0 +1,6 @@
+from sys import argv
+print("The Number of Command Line Arguments:",len(argv))
+print("The List of Command Line Arguments:",argv)
+print("Command Line Arguments one by one:")
+for x in argv:
+    print(x)
