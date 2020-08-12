40f420d11e8eeed3cde72412baed067f4081b3f8
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-12 13:01:10 +0530

    6th commit

---
+++ identity_is_operator.py
@@ -0,0 +1,11 @@
+a=10
+b=10
+print(a is b)
+
+list1=["one","two","three"]
+list2=["one","two","three"]
+print(id(list1))
+print(id(list2))
+print(list1 is list2)
+print(list1 is not list2)
+print(list1 == list2)
---
+++ membership_operator.py
@@ -0,0 +1,11 @@
+x="Hello...Learning Python is very very easy"
+print('h' in x)
+print('d' in x)
+print('d' not in x)
+print('Python' not in x)
+
+list1=["XYZ","QWERTY","ABCD"]
+print("QWERTY" in list1)
+print("abcd" in list1)
+print("XYZ" not in list1)
+print("ZZZ" not in list1)
