c1d0a1b951cccc15e3e99e9e17e27b4ce8867eec
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-14 19:51:48 +0530

    sample pgm committed

---
+++ average_of_3_numbers.py
@@ -0,0 +1,5 @@
+a=int(input("Enter First number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+
+print("Average=",(a+b+c)/3)
---
+++ swapping.py
@@ -0,0 +1,10 @@
+a=10
+b=22
+print("Before swaping first number is:",a)
+print("Before swaping second number is:",b)
+
+k=a
+a=b
+b=k
+print("After swaping first number is:",a)
+print("After swaping second number is:",b)
