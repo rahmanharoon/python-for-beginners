338cda9247e1f9a92f16cf775fac8e6ad5e3209d
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-15 18:06:36 +0530

    keys and values of dict

---
+++ dict_datatype/keys.py
@@ -0,0 +1,7 @@
+#keys() returns all the keys in dictionary
+
+d={100:"raju",200:"ravi",300:"shiva"}
+print(d)
+print(d.keys())
+for k in d.keys():
+    print(k)
---
+++ dict_datatype/values.py
@@ -0,0 +1,6 @@
+#values() return all the values contain in dictionary
+d={100:"raju",200:"ravi",300:"shiva"}
+print(d)
+print(d.values())
+for v in d.values():
+    print(v)
