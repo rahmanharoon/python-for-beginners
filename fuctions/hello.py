5d54cc1791e3c8f0c233f32bade9ce94ecbf6782
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-16 17:31:49 +0530

    fuctions samples

---
+++ fuctions/hello.py
@@ -0,0 +1,9 @@
+#syntax
+#def function_name(parameters) :
+#""" doc string"""
+
+def wish():
+    print("HEllo Morning")
+wish()
+wish()
+wish()    
---
+++ fuctions/printing_message.py
@@ -0,0 +1,4 @@
+def wish(name):
+    print("HEllo",name,"Morning")
+wish("Rahman")
+wish("Haroon")    
