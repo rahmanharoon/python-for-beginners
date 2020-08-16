c1ae8c30c189e88d7259aa37869d12eff6db4d33
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-16 17:37:30 +0530

    pgm for functions

---
+++ fuctions/squares.py
@@ -0,0 +1,4 @@
+def squarelt(number):
+    print("The square of",number,'is',number*number)
+squarelt(4)
+print(squarelt(3))    
---
+++ fuctions/sum.py
@@ -0,0 +1,5 @@
+def add(x,y):
+    return(x+y)
+result=add(10,20)
+print("Sum is:",result)
+print("Sum is",add(100,300))
