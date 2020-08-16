2c0ce91aba425d42552e1d67ebfb9db6920cbd9f
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-16 22:06:15 +0530

    factorial of a number

---
+++ fuctions/factorial.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    if n==0:
+        result=1
+    else:
+        result=n*factorial(n-1)
+    return result
+print("factorial of 4 is:",factorial(4))
+print("factorial of 5 is:",factorial(5))
