8c01f1457b05e1bcf0b133e1cca838d7e3a74b60
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-17 20:33:03 +0530

    string programs

---
+++ string/checking_string.py
@@ -0,0 +1,6 @@
+s=input("Enter Main string:")
+sub=input("Enter sub string:")
+if sub in s:
+    print(sub,"is found in main string!")
+else:
+    print(sub,"is not found in sub string!")
---
+++ string/comparing_string.py
@@ -0,0 +1,8 @@
+s1=input("Enter first string:")
+s2=input("Enter second string:")
+if s1==s2:
+    print("Strings are equal")
+elif s1>s2:
+    print("First string is greater than second string")
+else:
+    print("second string is greater than first string")
