26544a11af040d0b3e64e1d7065fbb4dcf87ff76
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-19 14:31:45 +0530

    member aliasing

---
+++ modules/member_aliasing.py
@@ -0,0 +1,8 @@
+from samplemath import x as y,add as sum
+print(y)
+sum(12,15)
+
+
+#print(x) Name Error ,x not defined
+#add(10,20) Name error , add not defined
+#Once we defined as alias name,we should use alias name only and we should not use original name 
