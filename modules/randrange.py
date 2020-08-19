3f6e713b829ed1b7f794af0a15630d4c9fd2f055
Rahman Haroon <rahmanharoon128@gmail.com>
2020-08-19 18:39:27 +0530

    printing a random number from range

---
+++ modules/randrange.py
@@ -0,0 +1,9 @@
+#returns a random number from range
+#start <= x < stop
+#start argument is optional and default value is 0
+#step argument is optional and default value is 1
+from random import *
+for i in range(20):
+    print(randrange(10))
+    print(randrange(1,11))
+    print(randrange(1,20,2))
